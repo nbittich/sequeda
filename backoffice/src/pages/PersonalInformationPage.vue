31c9c136e014e03ce2c3e11bf3c8efed6896ab89
Nordine Bittich <nordine@bittich.be>
2022-11-18 19:03:26 +0100

    proxy bs

--- backoffice/src/pages/PersonalInformationPage.vue
+++ backoffice/src/pages/PersonalInformationPage.vue
@@ -79,6 +79,7 @@
                 option-label="label"
                 option-value="value"
                 emit-value
+                map-options
                 label="Gender"
               />
             </div>
@@ -92,6 +93,7 @@
                 option-label="label"
                 option-value="value"
                 emit-value
+                map-options
               />
             </div>
           </div>
@@ -175,43 +177,74 @@
 
             <div class="col-lg-1 col-12 q-mb-xs-sm q-mb-md-none">
               <q-input
-              class="q-mr-md-xs"
-
+                class="q-mr-md-xs"
                 dense
                 outlined
                 v-model="current.contactDetail.address.boxNumber"
                 label="Box"
               />
             </div>
-
-            <div class="col-lg-1 col-12 q-mb-xs-sm q-mb-md-none">
-              <q-input
-              class="q-mr-md-xs"
-
+            <div class="col-lg-3 col-12 q-mb-xs-sm q-mb-md-none">
+              <q-select
+                class="q-mr-md-xs"
                 dense
                 outlined
-                v-model="current.contactDetail.address.postCode"
+                v-model="current.contactDetail.address.country"
+                use-input
+                option-label="label"
+                option-value="label"
+                emit-value
+                map-options
+                hide-selected
+                fill-input
+                input-debounce="0"
+                :options="countriesOptions"
+                @filter="filterCountry"
+                @input-value="setCountry"
+                label="Country"
+              >
+                <template v-slot:no-option>
+                  <q-item>
+                    <q-item-section class="text-grey">
+                      No results
+                    </q-item-section>
+                  </q-item>
+                </template>
+              </q-select>
+            </div>
+            <div class="col-lg-1 col-12 q-mb-xs-sm q-mb-md-none">
+              <q-select
+                class="q-mr-md-xs"
+                dense
+                outlined
+                :model-value="current.contactDetail.address.postCode"
+                use-input
+                :option-label="municipalityLabel"
+                hide-selected
+                fill-input
+                input-debounce="0"
+                :options="postalCodesOptions"
+                @filter="filterPostalCodes"
+                @input-value="setPostalCode"
                 label="Post code"
-              />
+              >
+                <template v-slot:no-option>
+                  <q-item>
+                    <q-item-section class="text-grey">
+                      No results
+                    </q-item-section>
+                  </q-item>
+                </template>
+              </q-select>
             </div>
             <div class="col-lg-3 col-12 q-mb-xs-sm q-mb-md-none">
               <q-input
-              class="q-mr-md-xs"
-
+                class="q-mr-md-xs"
+                :disable="true"
                 dense
                 outlined
                 v-model="current.contactDetail.address.municipality"
                 label="Municipality"
-              />
-            </div>
-            <div class="col-lg-3 col-12 q-mb-xs-sm q-mb-md-none">
-              <q-input
-              class="q-mr-md-xs"
-
-                dense
-                outlined
-                v-model="current.contactDetail.address.country"
-                label="Country"
               />
             </div>
           </div>
@@ -229,10 +262,12 @@
 
 <script lang="ts">
 import usePersonStore from 'src/stores/person';
-
+import useGeoStore, {  PostalCode } from 'src/stores/geoentities';
 import { defineComponent, ref } from 'vue';
 const personStore = usePersonStore();
+const geoStore = useGeoStore();
 await personStore.fetchCurrent();
+await geoStore.fetchCountries();
 
 export default defineComponent({
   name: 'PersonalInformation',
@@ -273,16 +308,87 @@ export default defineComponent({
   },
   async setup() {
     const current = ref(personStore.current);
-    return { current };
+    const countries = geoStore.countries;
+    const countriesOptions = ref(geoStore.countries);
+    const country = countries.find(
+      (c) => c.label === personStore.current.contactDetail.address.country
+    );
+    const municipality = personStore.current.contactDetail.address.municipality;
+    const selectedCountry = ref(country);
+    const selectedPostalCode = ref({
+      postalCode: personStore.current.contactDetail.address.postCode,
+      countryCode: country?.code || '',
+      name: municipality || '',
+    } as PostalCode);
+    const postalCodesOptions = ref(null as unknown as PostalCode[]);
+    return {
+      current,
+      countriesOptions,
+      selectedCountry,
+      postalCodesOptions,
+      selectedPostalCode,
+      municipalityLabel(opt: PostalCode | string) {
+        if(typeof opt ==="string" ) {
+            return opt;
+        }
+        return `${opt.postalCode} ${opt.name}`;
+      },
+      filterCountry(
+        val: string,
+        update: (arg0: () => void) => void,
+        abort: any
+      ) {
+        update(() => {
+          const needle = val.toLocaleLowerCase();
+          countriesOptions.value = countries.filter(
+            (v) => v.label?.toLocaleLowerCase()?.indexOf(needle) > -1
+          );
+        });
+      },
+      async filterPostalCodes(
+        val: string,
+        update: (arg0: () => void) => void,
+        abort: any
+      ) {
+        let postCodes:PostalCode[] = [];
+        if (selectedCountry.value) {
+          postCodes = await geoStore.postCodesByQuery(
+            selectedCountry.value,
+            val.trim()
+          );
+        }
+        update(() => {postalCodesOptions.value = postCodes});
+      },
+      setCountry(val: string) {
+        if (val) {
+          selectedCountry.value = countries.find((c) => c.label === val);
+          if(selectedPostalCode.value && selectedPostalCode.value.countryCode !== selectedCountry.value?.code) {
+            selectedPostalCode.value = null as unknown as PostalCode;
+            current.value.contactDetail.address.postCode = '';
+            current.value.contactDetail.address.municipality = '';
+          }
+        }
+      },
+      setPostalCode(val: string) {
+        console.log(val)
+        const option = postalCodesOptions.value?.find(p=>  `${p.postalCode} ${p.name}` === val || p.postalCode === val);
+        if (val && option) {
+          selectedPostalCode.value = option;
+          current.value.contactDetail.address.municipality = option.name;
+          current.value.contactDetail.address.postCode = option.postalCode;
+        }
+      },
+    };
   },
   methods: {
     async update() {
+      console.log(JSON.stringify(this.current));
       this.current = await personStore.update(this.current);
     },
     async reset(e: Event) {
       e.preventDefault();
-    }
-  }
+    },
+  },
 });
 </script>
 <style lang="sass" scoped></style>
